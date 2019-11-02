f518159ef0366ab2f46e941c26e6834cf84925b6
Ken Ito <mind.like.aqua@gmail.com>
2019-11-02 17:32:35 +0900

    Add 判定メソッド

--- src/Map.php
+++ src/Map.php
@@ -4,15 +4,14 @@ namespace Nagoyaphp\Dokaku18;
 
 class Map
 {
-    private $delimiter;
     private $values;
     private const HEX_LENGTH = 2;
     private const BIN_LENGTH = 8;
+    private const DELIMITER = '-';
 
-    public function __construct(string $input, string $delimiter = '-')
+    public function __construct(string $input)
     {
-        $this->delimiter = $delimiter;
-        $hexs = explode($this->delimiter, $input);
+        $hexs = explode(self::DELIMITER, $input);
         $this->values = array_map(function ($hex) {
             return hexdec($hex);
         }, $hexs);
@@ -20,25 +19,32 @@ class Map
 
     public function __toString()
     {
-        return implode($this->delimiter, array_map(function ($value) {
+        return implode(self::DELIMITER, array_map(function ($value) {
             return str_pad(dechex($value), self::HEX_LENGTH, '0', STR_PAD_LEFT);
         }, $this->values));
     }
 
-    public function removeFullLines($startPlace = 0)
+    public function removeFullLines()
     {
-        for ($place = $startPlace; $place < self::BIN_LENGTH; $place++) {
-            $placeValue = 2**$place;
-            if (empty(array_filter($this->values, function ($value) use ($placeValue) {
-                return !(bool)($placeValue & $value);
-            }))) {
+        $row = 0;
+        $removeCount = 0;
+        while ($row < (self::BIN_LENGTH - $removeCount)) {
+            if ($this->isFullLine($row)) {
                 foreach ($this->values as $key => $value) {
-                    $this->values[$key] = ($value % $placeValue) +
-                        (floor($value / ($placeValue * 2)) * $placeValue);
+                    $this->values[$key] =
+                        (($value >> ($row + 1)) << $row) + ($value & ((2**$row) - 1));
                 }
-                $this->removeFullLines($place);
-                return;
+                $removeCount++;
+            } else {
+                $row++;
             }
         }
+    }
+
+    private function isFullLine($row)
+    {
+        return empty(array_filter($this->values, function ($value) use ($row) {
+            return !(bool)((2**$row) & $value);
+        }));
     }
 }
